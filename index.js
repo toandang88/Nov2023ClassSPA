81a78294eceb0636bd58a451e032e83d51039d44
Toan Dang <toandangqc@gmail.com>
2024-01-03 20:53:04 -0600

    update for the server

--- index.js
+++ index.js
@@ -34,20 +34,49 @@ function afterRender(state) {
   }
 
   if (state.view === "Order") {
+    // Add an event handler for the submit button on the form
     document.querySelector("form").addEventListener("submit", event => {
       event.preventDefault();
 
-      store.Pizza.pizzas.push({
-        crust: event.target.elements.crust.value,
-        cheese: event.target.elements.cheese.value,
-        customer: event.target.elements.customer.value,
-        sauce: event.target.elements.sauce.value,
-        toppings: []
-      });
+      // Get the form element
+      const inputList = event.target.elements;
+      console.log("Input Element List", inputList);
 
-      console.log(store.Pizza.pizzas);
+      // Create an empty array to hold the toppings
+      const toppings = [];
 
-      router.navigate("/Pizza");
+      // Iterate over the toppings array
+
+      for (let input of inputList.toppings) {
+        // If the value of the checked attribute is true then add the value to the toppings array
+        if (input.checked) {
+          toppings.push(input.value);
+        }
+      }
+
+      // Create a request body object to send to the API
+      const requestData = {
+        customer: inputList.customer.value,
+        crust: inputList.crust.value,
+        cheese: inputList.cheese.value,
+        sauce: inputList.sauce.value,
+        toppings: toppings
+      };
+      // Log the request body to the console
+      console.log("request Body", requestData);
+
+      axios
+        // Make a POST request to the API to create a new pizza
+        .post(`${process.env.PIZZA_PLACE_API_URL}/pizzas`, requestData)
+        .then(response => {
+          //  Then push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+          store.Pizza.pizzas.push(response.data);
+          router.navigate("/Pizza");
+        })
+        // If there is an error log it to the console
+        .catch(error => {
+          console.log("It puked", error);
+        });
     });
   }
 }
